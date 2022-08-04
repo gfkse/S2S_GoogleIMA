1652c09b5e469244bd4a998789a6969d3ff39d9a
KornazovKristiyan <97964609+KornazovKristiyan@users.noreply.github.com>
2022-08-04 08:55:56 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
             targets: ["S2S_GoogleIMA"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/CbaronGFK/s2s.git" ,
+        .package(url: "https://github.com/gfkse/s2s_sdk.git" ,
                  from: "1.0.1"),
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
