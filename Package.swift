261e40894ec54cc1da4bb68fd19c008a1c1c04d0
Christian Baron <christianbaron@MacBook-Pro-von-Christian.local>
2022-08-04 11:52:23 +0200

    Update

---
+++ Package.swift
@@ -0,0 +1,28 @@
+// swift-tools-version: 5.6
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "S2S_GoogleIMA",
+    platforms: [.iOS(.v9)],
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "S2S_GoogleIMA",
+            targets: ["S2S_GoogleIMA"])
+    ],
+    dependencies: [
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on
+        .binaryTarget(
+             name: "S2S_GoogleIMA",
+             url:
+               "https://s3.eu-central-1.amazonaws.com/download.sensic.net/s2s/sdk/ios/testing/1/S2S_GoogleIMA_2.zip",
+             checksum: "7ce28955feea4286b4ab4865aa143a09696bfd48cfd60d6a192828aed19ccadd"
+           ),
+
+    ]
+)
