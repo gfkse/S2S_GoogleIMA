f6ea4e57bb092a6b970c9a7280ff489fe36fb817
KornazovKristiyan <97964609+KornazovKristiyan@users.noreply.github.com>
2022-08-05 11:31:21 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -10,7 +10,6 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "S2S_GoogleIMA",
-            type: .dynamic,
             targets: ["S2S_GoogleIMA"])
     ],
     dependencies: [
