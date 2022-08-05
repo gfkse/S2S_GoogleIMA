125f646ae2e9d53075084935b22517685a0c9db9
Bamboo <bamboo@mbpsensooagent2.gfk.com>
2022-08-05 07:50:22 +0200

    Update

--- Package.swift
+++ Package.swift
@@ -21,7 +21,7 @@ let package = Package(
              name: "S2S_GoogleIMA",
              url:
                "https://s3.eu-central-1.amazonaws.com/download.sensic.net/s2s/sdk/ios/testing/1/S2S_GoogleIMA_1.zip",
-             checksum: "8cf4b001a6af5e40bd02a7b46bc79d90c948d59f97777b7138d2e0cc1bac2d95"
+             checksum: "116603a67376a7c9dc27c4eda8a7823ad41d349b022a4596b3de2bddef866f91"
            ),
 
     ]
